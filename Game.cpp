3d88fcad040abd9b5eba68c9dbc3b254afbd6bae
Andy Situ <and.situ@gmail.com>
2019-05-04 21:16:46 -0700

    Update game, fixed prev errors

--- Game.cpp
+++ Game.cpp
@@ -35,15 +35,17 @@ void Game::start_game()
     board.create_board(row, col);
 
     bool correct_num_bugs = false;
+    int ants_input;
+    int doodlebugs_input;
 
     while (!correct_num_bugs) { // Make sure that # bugs does not exceed board
         std::cout << "How many ants would you like?" << std::endl;
-        num_ants = Helper::getIntInput(10, 100);
+        ants_input = Helper::getIntInput(10, 100);
 
         std::cout << "How many doodlebugs would you like?" << std::endl;
-        num_doodlebugs = Helper::getIntInput(10, 100);
+        doodlebugs_input = Helper::getIntInput(10, 100);
 
-        if ((num_doodlebugs * num_ants) <= (row * col)) {
+        if ((doodlebugs_input * ants_input) <= (row * col)) {
             correct_num_bugs = true;
         }
         else {
@@ -52,13 +54,13 @@ void Game::start_game()
         }
     }
 
-    ants_array_size = num_ants * 2;
-    doodlebug_array_size = num_doodlebugs * 2;
+    ants_array_size = ants_input * 2;
+    doodlebug_array_size = doodlebugs_input * 2;
 
     ants = new Ant*[ants_array_size];
     doodlebugs = new Doodlebug*[doodlebug_array_size];
 
-    for (int i = 0; i < num_ants; i++) {
+    for (int i = 0; i < ants_input; i++) {
         bool found_empty_spot = false;
         while (!found_empty_spot) {
             int random_row = Helper::randInt(0, row - 1),
@@ -67,13 +69,10 @@ void Game::start_game()
                 create_ant(random_row, random_col);
                 found_empty_spot = true;
             }
-            board.print_board();
-            std::cout << random_row << random_col << std::endl << found_empty_spot;
         }
     }
-    std::cout << "d";
     
-    for (int i = 0; i < num_doodlebugs; i++) {
+    for (int i = 0; i < doodlebugs_input; i++) {
         bool found_empty_spot = false;
         while (!found_empty_spot) {
             int random_row = Helper::randInt(0, row - 1),
@@ -96,7 +95,8 @@ void Game::create_ant(int row, int col)
     num_ants++;
 
     if (num_ants > ants_array_size) {
-        std::cout << "TEST";
+
+        std::cout << num_ants << "ANTS" << ants_array_size;
         int old_arr_size = ants_array_size;
         ants_array_size = num_ants * 2;
         Ant ** new_ants = new Ant*[ants_array_size];
@@ -119,7 +119,7 @@ void Game::create_doodlebug(int row, int col)
     num_doodlebugs++;
 
     if (num_doodlebugs > doodlebug_array_size) {
-        std::cout << "TEST";
+        std::cout << "D" << doodlebug_array_size;
         int old_arr_size = doodlebug_array_size;
         doodlebug_array_size = num_doodlebugs * 2;
         Doodlebug ** new_doodlebugs = new Doodlebug*[doodlebug_array_size];
