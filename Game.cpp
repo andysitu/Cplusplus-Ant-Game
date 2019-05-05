40bfab14a2726c3f79350cba7710f86ddbc82ac3
Andy Situ <and.situ@gmail.com>
2019-05-04 17:46:32 -0700

    add to Game

--- Game.cpp
+++ Game.cpp
@@ -12,12 +12,14 @@
 *************************************************************/
 Game::Game()
 {
+    Helper::set_srand();
     start_game();
 }
 
 
 /*************************************************************
-* 
+* Starts the game itself, asking user for board size
+* and # critters and then complets the game
 *************************************************************/
 void Game::start_game()
 {
@@ -32,4 +34,36 @@ void Game::start_game()
 
     board.create_board(row, col);
 
+    bool correct_num_bugs = false;
+
+    int num_ants;
+    int num_doodlebugs;
+
+    while (!correct_num_bugs) { // Make sure that # bugs does not exceed board
+        std::cout << "How many ants would you like?" << std::endl;
+        num_ants = Helper::getIntInput(10, 100);
+
+        std::cout << "How many doodlebugs would you like?" << std::endl;
+        num_doodlebugs = Helper::getIntInput(10, 100);
+
+        if ((num_doodlebugs * num_ants) <= (row * col)) {
+            correct_num_bugs = true;
+        }
+        else {
+            std::cout << "You wanted more bugs than there are board spaces available." << std::endl;
+            std::cout << "Please enter the values again." << std::endl;
+        }
+    }
+
+    // Randomly place bugs until they're completely placed on board
+    int ants_placed = 0,
+        doodlebugs_placed = 0;
+
+    for (int i = 0; i < num_ants; i++) {
+        
+    }
+
+    for (int i = 0; i < num_doodlebugs; i++) {
+        
+    }
 }
